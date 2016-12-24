b51c5b3a5a2de225543da5dad330e11e4f12d616
Amandeep Singh <amandeep4348@gmail.com>
2016-12-25 01:00:28 +0530

    Add files via upload

---
+++ Scripts/InstantVelocity.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InstantVelocity : MonoBehaviour {
+
+	public Vector2 velocity = Vector2.zero;
+	private Rigidbody2D body2D;
+	void Awake () {
+		body2D = GetComponent<Rigidbody2D> ();
+	}
+
+	void FixedUpdate () {
+		body2D.velocity = velocity;
+	}
+}
---
+++ Scripts/Jump.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Jump : MonoBehaviour {
+	public float jumpSpeed = 240f;
+	public float forwardSpeed = 20f;
+	private Rigidbody2D body2D;
+	private InputState inputState;
+
+	void Awake(){
+		body2D = GetComponent<Rigidbody2D> ();
+		inputState = GetComponent<InputState> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (inputState.standing) {
+			if (inputState.actionButton) {
+				body2D.velocity = new Vector2 (transform.position.x < 0 ? forwardSpeed : 0, jumpSpeed);
+			}
+		}
+	}
+}
